f1d060fe1f2218e0e5200f0ae1a67d112e323ebf
zachnewburgh <zach.newburgh@gmail.com>
2016-11-04 13:09:41 -0400

    Configuring states with single home state

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,2 +1,11 @@
 angular
   .module('app', ['ui.router', 'ngResource'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.html',
+        controller: 'HomeController as ctrl'
+      });
+    $urlRouterProvider.otherwise('/');
+  });
