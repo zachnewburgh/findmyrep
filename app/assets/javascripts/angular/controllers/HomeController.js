498f7fac7e1d410de492ac1819daaf184dfaaa40
zachnewburgh <zach.newburgh@gmail.com>
2016-11-06 11:06:11 -0500

    Changed urls to include https

--- app/assets/javascripts/angular/controllers/HomeController.js
+++ app/assets/javascripts/angular/controllers/HomeController.js
@@ -14,7 +14,7 @@ HomeController = ['APIService', '$http', function(APIService, $http) {
           $http.get("https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=" + p.first_name + " " + p.last_name + "&format=json")
             .then(function(response) {
               title = response.data.query.search[0].title;
-              $http.get("http://en.wikipedia.org/w/api.php?origin=*&action=query&prop=images&format=json&titles=" + title)
+              $http.get("https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=images&format=json&titles=" + title)
                 .then(function(response) {
                   arr = [];
                   images = Object.values(response.data.query.pages)[0].images
@@ -23,7 +23,7 @@ HomeController = ['APIService', '$http', function(APIService, $http) {
                     img.title.search(regex) !== -1 ? arr.push(img.title) : null;
                   })
                   image = arr[0];
-                  $http.get("http://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&titles=" + image + "&prop=imageinfo&iiprop=url")
+                  $http.get("https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&titles=" + image + "&prop=imageinfo&iiprop=url")
                     .then(function(response) {
                       p.image_url = Object.values(response.data.query.pages)[0].imageinfo[0].url
                     })
